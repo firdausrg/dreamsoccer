17185ecf9942e16a507ad5b48a8c5fc2683e3107
Pirdaus Ripaatullah Gopur <firdausrifa48@gmail.com>
2025-01-26 16:04:56 +0700

    test

---
+++ src/main/java/com/promptengineer/dreamsoccer/model/lapangan.java
@@ -0,0 +1,15 @@
+package com.promptengineer.dreamsoccer.model;
+
+/*
+Created By IntelliJ IDEA 2024.3 (Community Edition)
+Build #IC-243.21565.193, built on November 13, 2024
+@Author pirda Pirdaus Ripa Atullah Gopur
+Created on 26/01/2025 15:46
+@Last Modified 26/01/2025 15:46
+Version 1.0
+*/
+public class lapangan {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
